a0737ba5fb870a6c21e60797fb34cbad45acb1bb
HARSH JAIN <jainharsh270@gmail.com>
2019-10-19 15:02:39 +0530

    release apks added

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,9 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:intl/intl.dart';
 
-
-void main() async{
-
+void main() async {
   Map _data = await getJson();
   List _features = _data['features'];
-
-
-
 
   runApp(new MaterialApp(
     home: new Scaffold(
@@ -21,78 +16,83 @@ void main() async{
         centerTitle: true,
         backgroundColor: Colors.orangeAccent,
       ),
-
       body: new Center(
         child: new ListView.builder(
           itemCount: _features.length,
           padding: const EdgeInsets.all(16.0),
-          itemBuilder: (BuildContext context,int position){
-            if(position.isOdd) return new Divider();
+          itemBuilder: (BuildContext context, int position) {
+            if (position.isOdd) return new Divider();
 
-            final index = position ~/ 2; 
+            final index = position ~/ 2;
 
             var format = new DateFormat('dd-MM-yyyy').add_jm();
 
             var date = format.format(new DateTime.fromMicrosecondsSinceEpoch(
-              _features[index]['properties']['time']*1000,
+              _features[index]['properties']['time'] * 1000,
               isUtc: false,
             ));
 
             return new ListTile(
-              title: new Text("$date",
-              style: new TextStyle(
-                fontSize: 13.4,
-                color: Colors.blue.shade900,
-                fontWeight: FontWeight.w900,
-              ),),
-              subtitle: new Text("${_features[index]['properties']['place']}",
-              style: new TextStyle(
-                fontSize: 12.4,
-                color: Colors.lightBlue,
-                fontStyle: FontStyle.italic,
-                fontWeight: FontWeight.w500
-              ),),
-
+              title: new Text(
+                "$date",
+                style: new TextStyle(
+                  fontSize: 13.4,
+                  color: Colors.blue.shade900,
+                  fontWeight: FontWeight.w900,
+                ),
+              ),
+              subtitle: new Text(
+                "${_features[index]['properties']['place']}",
+                style: new TextStyle(
+                    fontSize: 12.4,
+                    color: Colors.lightBlue,
+                    fontStyle: FontStyle.italic,
+                    fontWeight: FontWeight.w500),
+              ),
               leading: new CircleAvatar(
                 backgroundColor: Colors.green,
-                child: new Text("${_features[index]['properties']['mag']}",
-                style: new TextStyle(
-                  fontSize: 18.4,
-                  color: Colors.white,
-                  fontWeight: FontWeight.w500,
-                ),),
+                child: new Text(
+                  "${_features[index]['properties']['mag']}",
+                  style: new TextStyle(
+                    fontSize: 18.4,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
               ),
-
-              onTap: () {_showOnTapMessage(context, "${_features[index]['properties']['title']}");},
+              onTap: () {
+                _showOnTapMessage(
+                    context, "${_features[index]['properties']['title']}");
+              },
             );
           },
         ),
       ),
-
     ),
   ));
 }
 
-
-Future<Map> getJson() async{
-  String apiUrl = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson';
+Future<Map> getJson() async {
+  String apiUrl =
+      'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson';
 
   http.Response response = await http.get(apiUrl);
-  return json.decode(response.body); 
+  return json.decode(response.body);
 }
 
-void _showOnTapMessage(BuildContext context,String message){
-
+void _showOnTapMessage(BuildContext context, String message) {
   var alert = new AlertDialog(
     title: new Text("Quake Alert!"),
     content: new Text(message),
     actions: <Widget>[
       new FlatButton(
-        onPressed: (){Navigator.pop(context);},
+        onPressed: () {
+          Navigator.pop(context);
+        },
         child: new Text("OK"),
       )
     ],
   );
 
-  showDialog(context: context,builder: (context) => alert);
+  showDialog(context: context, builder: (context) => alert);
 }
